ac6d81bebb6bd0b6aebeff14ad081f4ba5c2560a
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-03-07 12:19:42 +0100

    Add multithreading message on websockets

--- python_server/websocket_server.py
+++ python_server/websocket_server.py
@@ -3,37 +3,40 @@
 import asyncio
 import websockets
 import json
+import time
 from threading import Thread
 
 
 class Websocket_server(Thread):
 
     def __init__(self, port=3000):
-        Thread.__init__(self, daemon = True)
+        Thread.__init__(self)
         self.port = port
-        
-
-    async def hello(self, websocket, path):
-        with open("./menu.json") as f:
-            await websocket.send(f.read())
-        with open("./example.json") as f:
-            data = json.load(f)
-            for message in data:
-                await websocket.send(json.dumps({"message": message}))
-                await asyncio.sleep(1)
-
-        command = None
-        while(command != "quit"):
-            command = await websocket.recv()
-            print("Receive: {}".format(command))
-        websocket.close()
-        exit()
+        self.connected = set()
+        self.showMustGoOn = True
+        self.dataToSend = []
 
     def run(self):
         self.loop = asyncio.new_event_loop()
 
-        start_server = websockets.serve(self.hello, 'localhost', self.port, loop=self.loop)
+        start_server = websockets.serve(self.handler, 'localhost', self.port, loop=self.loop)
 
         self.loop.run_until_complete(start_server)
         self.loop.run_forever()
+
+    async def handler(self, websocket, path):
+        self.connected.add(websocket)
+        try:
+            await websocket.recv()
+        except websockets.exceptions.ConnectionClosed:
+            pass
+        finally:
+            self.connected.remove(websocket)
+
+    def sendData(self, data):
+        for websocket in self.connected.copy():
+            print("Sending data: %s" % data)
+            coro = websocket.send(data)
+            future = asyncio.run_coroutine_threadsafe(coro, loop)
+
 
