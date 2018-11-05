5f13aa9565369d434d1d3178a530b1069e22802c
Antoine du HAMEL <duhamelantoine1995@gmail.com>
2018-11-04 21:22:07 -0500

    v0.3.1

--- setup.py
+++ setup.py
@@ -5,20 +5,24 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="web_logger",
-    version="0.3.0",
+    version="0.3.1",
     author="Antoine du Hamel",
     author_email="duhamelantoine1995@gmail.com",
     description="Logger from python to HTML",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
     python_requires=">=3.5",
     url="https://github.com/aduh95/web_logger",
     packages=["web_logger"],
     install_requires=["websockets"],
+    project_urls={
+        "Bug Tracker": "https://github.com/aduh95/web_logger/issues",
+        "Source Code": "https://github.com/aduh95/web_logger",
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
 )
-
