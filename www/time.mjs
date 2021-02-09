b060357712d5329265f4b1ca96e1a161692b7ad7
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-02-09 19:37:31 +0100

    Avoid using setInterval to update <time>

--- www/time.mjs
+++ www/time.mjs
@@ -1,25 +1,38 @@
+function scheduleNext(time) {
+  const elapsed = time - this._start;
+  const roundedElapsed = Math.round(elapsed / 1000) * 1000;
+  const targetNext = this._start + roundedElapsed + 1000;
+  this._timeout = setTimeout(
+    () => (this._raf = requestAnimationFrame(this._scheduleNext)),
+    targetNext - performance.now()
+  );
+
+  this.updateDate();
+}
+
 class AutoUpdateTimeElement extends HTMLTimeElement {
+  constructor() {
+    super();
+    this._scheduleNext = scheduleNext.bind(this);
+  }
+
   connectedCallback() {
     if (this.isConnected) {
-      clearInterval(this._updateDateID);
-      this._updateDateID = setInterval(() => {
-        this.updateDate();
-      }, 1000);
+      this.disconnectedCallback();
+      this._scheduleNext((this._start = performance.now()));
     }
   }
 
   disconnectedCallback() {
-    clearInterval(this._updateDateID);
+    clearTimeout(this._timeout);
+    cancelAnimationFrame(this._raf);
   }
 
   /**
    * Sets the date into an element
    */
   updateDate() {
-    cancelAnimationFrame(this._animationFrame);
-    this._animationFrame = requestAnimationFrame(() => {
-      this.textContent = new Date().toLocaleTimeString();
-    });
+    this.textContent = new Date().toLocaleTimeString();
   }
 }
 
